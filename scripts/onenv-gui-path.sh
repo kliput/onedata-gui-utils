18336d5c9e340b3f21e9b26971a4ddd97eecff42
Jakub Liput <jakub.liput@gmail.com>
2018-06-06 09:11:04 +0200

    Added node number to onenv-gui-path

--- scripts/onenv-gui-path.sh
+++ scripts/onenv-gui-path.sh
@@ -4,9 +4,11 @@
 [ -z ${NODE_PREFIX+x} ] && NODE_PREFIX=onezone
 # one of: op-worker, op-panel, oz-worker, oz-panel
 [ -z ${SERVICE_TYPE+x} ] && SERVICE_TYPE=oz-panel
+# one of: op-worker, op-panel, oz-worker, oz-panel
+[ -z ${NODE_NUMBER+x} ] && NODE_NUMBER=1
 BASE_DIR=$HOME/.one-env/deployments
 DEPLOYMENT_DIR=`ls -t $HOME/.one-env/deployments/ | head -1`
-echo $BASE_DIR/$DEPLOYMENT_DIR/$NODE_PREFIX/$NODE_PREFIX-node-1-$SERVICE_TYPE-rel/data/gui_static
+echo $BASE_DIR/$DEPLOYMENT_DIR/$NODE_PREFIX/$NODE_PREFIX-node-$NODE_NUMBER-$SERVICE_TYPE-rel/data/gui_static
 
 # NOTE: known paths
 # onezone/onezone-node-1-oz-worker-rel
