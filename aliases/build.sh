b90c46dd506ae4adddccd928b73db5d6b98f74e3
Jakub Liput <jakub.liput@gmail.com>
2019-02-20 16:54:28 +0100

    Mock build aliases

---
+++ aliases/build.sh
@@ -0,0 +1,9 @@
+# Please set Onedata Gui Utils Dir first using in shell:
+export OGU_DIR=/Users/kliput/Onedata/onedata-gui-utils
+export OG_MOCK_DIR=/Users/kliput/Onedata/onedata-gui-server-mock
+export OD_DIR=/Users/kliput/Onedata
+
+alias eb-panel-mock="cd ${OD_DIR}/onepanel-gui/src && ember build --environment=development --output-path=${OG_MOCK_DIR}/static/onedata-gui-static/onepanel --watch"
+alias eb-zone-mock="cd ${OD_DIR}/onezone-gui/src && ember build --environment=development --output-path=${OG_MOCK_DIR}/static/onedata-gui-static/oz/onezone --watch"
+alias eb-provider-mock="cd ${OD_DIR}/op-gui-default/src && ember build --environment=development --output-path=${OG_MOCK_DIR}/static/onedata-gui-static/op/oneprovider-1 --watch"
+
